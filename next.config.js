a1234ecaaa011e5e778b1f1c9a36964f5a3bcc3f
iowercase-i <iowercase-i@protonmail.com>
2025-11-14 09:23:45 -0800

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Static export for Cloudflare Pages
+  images: {
+    unoptimized: true, // Required for static export
+  },
 }
 
 module.exports = nextConfig
